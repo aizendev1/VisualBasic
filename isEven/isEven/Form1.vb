68a5aa9e5776e962ab5786deeac284143aa18009
aizendev1 <aizendev1@gmail.com>
2020-02-13 02:54:52 -0800

    isEven
    A VB program that  checks if a number is even  or odd

---
+++ isEven/isEven/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Dim Number1 As Integer
+    Function Even(ByRef N1)
+        If N1 Mod 2 = 0 Then
+            ListBox1.Items.Add("The number you enetered is even")
+        Else
+            ListBox1.Items.Add("The number you entered is odd")
+        End If
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Number1 = TextBox1.Text
+        Even(Number1)
+    End Sub
+End Class
