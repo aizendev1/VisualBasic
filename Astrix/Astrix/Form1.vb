905963113739d7dc154f7c361f181a74187f2fb6
aizendev1 <aizendev1@gmail.com>
2020-02-13 02:41:57 -0800

    Solution
     (Write a sub procedure that displays as the solid square of asterisks whose side is specified in integer parameter size)

---
+++ Astrix/Astrix/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Dim Number As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Sub CreateDisplay(ByRef V1)
+        Dim counter As Integer = 0
+        Dim sidecouner As Integer = 0
+        While counter < V1
+            counter += 1
+            While sidecouner < V1
+                sidecouner += 1
+
+            End While
+            ListBox1.Items.Add(StrDup(sidecouner, "*"))
+        End While
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Number = TextBox1.Text
+        CreateDisplay(Number)
+    End Sub
+End Class
